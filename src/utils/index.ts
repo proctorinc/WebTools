0ed1e126cd58a068951f488a4e11d599b9165852
Matt Proctor <proctormatt0@gmail.com>
2023-03-11 22:09:04 -0800

    Moved formatHsv function to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,12 @@
+import { Colord } from "colord";
+
 export const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text);
+};
+
+export const formatHsv = (color: Colord) => {
+  const hsv = color.toHsv();
+  return `hsv${hsv.a !== 1 ? "a" : ""}(${hsv.h}, ${hsv.s}%, ${hsv.v}%${
+    hsv.a !== 1 ? ` ${hsv.a}` : ""
+  })`;
 };
