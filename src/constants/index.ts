d62847c86002176694077885a5a6a03988864a9c
Matt Proctor <proctormatt0@gmail.com>
2023-03-11 20:22:40 -0800

    Added contants for tool categories

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const DATA_TOOL = "Data Tools";
+export const CODE_GENERATOR_TOOL = "Code Generators";
+export const MISC_TOOL = "Misc";
+
+export enum TOOL_CATEGORIES {
+  DATA_TOOL,
+  CODE_GENERATOR_TOOL,
+  MISC_TOOL,
+}
