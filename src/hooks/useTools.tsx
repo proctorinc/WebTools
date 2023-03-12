9fc55596fb00d022dfd98956e9b426dfc0735b7a
Matt Proctor <proctormatt0@gmail.com>
2023-03-11 20:28:05 -0800

    Reworked useTools to import tool features and store in different categories

--- src/hooks/useTools.tsx
+++ src/hooks/useTools.tsx
@@ -1,92 +1,32 @@
-import {
-  CropTwoTone,
-  DarkModeTwoTone,
-  DataObjectTwoTone,
-  GradientTwoTone,
-  Inbox,
-  PaletteTwoTone,
-  WallpaperTwoTone,
-} from "@mui/icons-material";
-import { BaseEncoder, ColorPicker, PrettyJson } from "../features/data";
+import { useMemo } from "react";
+import { CODE_GENERATOR_TOOL, DATA_TOOL, MISC_TOOL } from "../constants";
+import { BaseEncoderTool } from "../features/BaseEncoder";
+import { ColorPickerTool } from "../features/ColorPicker";
+import { JsonPrettyTool } from "../features/PrettyJson";
 
-type useToolsProps = {
-  urlParameter: string | undefined;
-};
+export const useTools = () => {
+  const tools = [BaseEncoderTool, ColorPickerTool, JsonPrettyTool];
 
-export const useTools = (props: useToolsProps) => {
-  const { urlParameter } = props;
-
-  const CATEGORIES = [
-    {
-      title: "Data Tools",
-      tools: [
-        {
-          title: "JSON Prettifier",
-          url: "json-prettifier",
-          description:
-            "Formats valid JSON into pretty output. Change the formatting parameters and copy to clipboard!",
-          icon: <DataObjectTwoTone sx={{ height: 20 }} />,
-          content: <PrettyJson />,
-        },
-        {
-          title: "Color Picker",
-          url: "color-picker",
-          description:
-            "Select, customize, and tweak colors in Hex, RGB, HSL, and HSV formats",
-          icon: <PaletteTwoTone sx={{ height: 20 }} />,
-          content: <ColorPicker />,
-        },
-        {
-          title: "Base-N Encoding",
-          url: "base-encoding",
-          description: "",
-          icon: <Inbox sx={{ height: 20 }} />,
-          content: <BaseEncoder />,
-        },
-      ],
-    },
-    {
-      title: "Code Generators",
-      tools: [
-        {
-          title: "CSS Gradients",
-          url: "css-gradients",
-          description: "",
-          icon: <GradientTwoTone sx={{ height: 20 }} />,
-          content: <div>CSS Gradients</div>,
-        },
-        {
-          title: "Tailwind Theme",
-          url: "tailwind-theme",
-          description: "",
-          icon: <DarkModeTwoTone sx={{ height: 20 }} />,
-          content: <div>Tailwind Theme</div>,
-        },
-      ],
-    },
-    {
-      title: "Misc",
-      tools: [
-        {
-          title: "Image Cropper",
-          url: "image-cropper",
-          description: "",
-          icon: <CropTwoTone sx={{ height: 20 }} />,
-          content: <div>Image Cropper</div>,
-        },
-        {
-          title: "Background Remover",
-          url: "background-remover",
-          description: "",
-          icon: <WallpaperTwoTone sx={{ height: 20 }} />,
-          content: <div>Background Remover</div>,
-        },
-      ],
-    },
-  ];
+  const categorizedTools = useMemo(
+    () => [
+      {
+        title: DATA_TOOL,
+        tools: tools.filter((tool) => tool.type === DATA_TOOL),
+      },
+      {
+        title: CODE_GENERATOR_TOOL,
+        tools: tools.filter((tool) => tool.type === CODE_GENERATOR_TOOL),
+      },
+      {
+        title: MISC_TOOL,
+        tools: tools.filter((tool) => tool.type === MISC_TOOL),
+      },
+    ],
+    []
+  );
 
   return {
-    tools: CATEGORIES,
-    firstTool: CATEGORIES[0].tools[0],
+    tools,
+    categorizedTools,
   };
 };
